d85248fe2ef9d18c4c6f9783ea809493bc092d96
100001010 <eric93522>
2025-09-01 12:05:40 +0800

    收盤改用收盤價不是用結算價

--- daily/add_column.py
+++ daily/add_column.py
@@ -9,18 +9,21 @@
 收高收低，是拿今天結算跟昨天結算比
     c_position:(h,l)
 
+重要：
+    策略是用收盤價，但是xq是用結算價
+    所以這裡先用收盤價
 
 '''
 import pandas as pd
 
 df = pd.read_csv("filtered_all.csv")
-df['k_type'] = df.apply(lambda row: 'r' if row['settlement'] >= row['open'] else 'g', axis=1)
+df['k_type'] = df.apply(lambda row: 'r' if row['close'] >= row['open'] else 'g', axis=1)
 
 # 欄位裡沒有昨日結算價
 for i in range(1, len(df)):
-    df.at[i, 'prev_close'] = df.at[i-1, 'settlement']
+    df.at[i, 'prev_close'] = df.at[i-1, 'close']
 
 df['o_position'] = df.apply(lambda row: 'h' if row['open'] >= row['prev_close'] else 'l', axis=1)
-df['c_position'] = df.apply(lambda row: 'h' if row['settlement'] >= row['prev_close'] else 'l', axis=1)
+df['c_position'] = df.apply(lambda row: 'h' if row['close'] >= row['prev_close'] else 'l', axis=1)
 
 df.to_csv("filtered_all_with_columns.csv", index=False)
